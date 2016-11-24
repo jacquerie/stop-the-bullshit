a5f75011063e8c66ad4ff858e00ca7ebd5c9e2f6
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2016-11-24 08:35:53 +0100

    facebook: show user name

--- src/js/facebook.js
+++ src/js/facebook.js
@@ -128,12 +128,13 @@ $(document).ready(function () {
           cleanURL = url.split('|')[0];
 
         if (blockedURLs.has(cleanURL)) {
-          var $blockedPost = $(this).closest('.userContentWrapper');
+          var $blockedPost = $(this).closest('.userContentWrapper'),
+            $userLink = $blockedPost.find('h5 a:first-child()');
 
           $blockedPost.fadeOut(400, function () {
-            $blockedPost.html('<p class="suggestion"> \
-              This user posted something which is probably false. Inform them \
-              that <b>they are helping a scammer</b> reach more people, and \
+            $blockedPost.html('<p class="suggestion">' + $userLink.text() +
+              ' posted something which is probably false. Inform them that \
+              <b>they are helping a scammer</b> reach more people, and \
               recommend them to <a href="https://goo.gl/W3i1Jj">install \
               StopTheBullshit</a>.</p>');
             $blockedPost.fadeIn();
